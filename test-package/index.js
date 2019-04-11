31dda1dd04ac86a492739f58dd1b06fc172b8286
hasparus <hasparus@gmail.com>
2019-04-12 00:13:49 +0200

    💚 Use template for test-package package.json
    I had lots of troubles with Travis build. Trying a workaround.

--- test-package/index.js
+++ test-package/index.js
@@ -8,46 +8,48 @@ const exec = promisify(childProcess.exec);
 
 const execHere = command => exec(`cd ${__dirname} && ${command}`);
 
-const packageName = "slash-cli";
-
 async function main() {
-  ["node_modules", "yarn.lock", "../*.tgz"].forEach(s => {
-    rimraf.sync(join(__dirname, s));
-  });
+  try {
+    ["node_modules", "yarn.lock", "../*.tgz"].forEach(s => {
+      rimraf.sync(join(__dirname, s));
+    });
 
-  const up = join(__dirname, "..");
+    const up = join(__dirname, "..");
 
-  await exec(`cd ${up} && yarn pack`);
-  let { stdout: lsResult } = await exec(`ls ${up}/*.tgz`);
+    await exec(`cd ${up} && yarn pack`);
+    let { stdout: lsResult } = await exec(`ls ${up}/*.tgz`);
 
-  const tgzPath = relative(__dirname, lsResult.trim());
+    const tgzPath = relative(__dirname, lsResult.trim());
 
-  await execHere("yarn");
-  await execHere(`yarn remove ${packageName}`);
-  await execHere(`yarn add ${tgzPath}`);
+    await execHere("cp ./package.template.json ./package.json");
+    await execHere("yarn");
+    await execHere(`yarn add ${tgzPath}`);
 
-  const testArgs = [
-    "src\\components\\App.js",
-    "src\\components\\Button.js",
-    "foobar",
-    "one/two",
-  ];
+    const testArgs = [
+      "src\\components\\App.js",
+      "src\\components\\Button.js",
+      "foobar",
+      "one/two",
+    ];
 
-  result = await execHere(`yarn slash ${testArgs.join(" ")}`);
+    result = await execHere(`yarn slash ${testArgs.join(" ")}`);
 
-  assert.strictEqual(result.stderr, "", "stderr is not empty");
+    assert.strictEqual(result.stderr, "", "stderr is not empty");
 
-  const actual = result.stdout
-    .split("\n")
-    .filter(Boolean)
-    .slice(-testArgs.length);
+    const actual = result.stdout
+      .split("\n")
+      .filter(Boolean)
+      .slice(-testArgs.length);
 
-  assert.deepStrictEqual(actual, [
-    "src/components/App.js",
-    "src/components/Button.js",
-    "foobar",
-    "one/two",
-  ]);
+    assert.deepStrictEqual(actual, [
+      "src/components/App.js",
+      "src/components/Button.js",
+      "foobar",
+      "one/two",
+    ]);
+  } finally {
+    await execHere("rm ./package.json");
+  }
 }
 
 main()
